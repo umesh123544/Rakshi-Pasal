42b72777947ad77b401ae8b22268b99a0e036b4b
umesh123544 <umesh.saral88@gmail.com>
2025-05-22 20:54:06 +0545

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -107,3 +107,116 @@ exports.getUser = async (req, res) => {
     res.status(500).send('Server error');
   }
 };
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
+exports.register = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    
+    const newUser = await User.create({
+      username,
+      password
+    });
+
+    const token = signToken(newUser._id);
+
+    res.status(201).json({
+      status: 'success',
+      token,
+      data: {
+        user: newUser
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    // 1) Check if username and password exist
+    if (!username || !password) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Please provide username and password'
+      });
+    }
+
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ username }).select('+password');
+
+    if (!user || !(await user.comparePassword(password, user.password))) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'Incorrect username or password'
+      });
+    }
+
+    // 3) If everything ok, send token to client
+    const token = signToken(user._id);
+
+    res.status(200).json({
+      status: 'success',
+      token,
+      data: {
+        user
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
+};
+
+exports.protect = async (req, res, next) => {
+  try {
+    // 1) Getting token and check if it's there
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'You are not logged in! Please log in to get access.'
+      });
+    }
+
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'The user belonging to this token does no longer exist.'
+      });
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+    next();
+  } catch (err) {
+    res.status(401).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
+};
