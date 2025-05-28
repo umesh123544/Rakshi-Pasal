8cf42218d6f64c49021067ebaf062a97b5f9f653
umesh123544 <umesh.saral88@gmail.com>
2025-05-28 13:51:14 +0545

    port

--- Backend/server.js
+++ Backend/server.js
@@ -36,7 +36,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
