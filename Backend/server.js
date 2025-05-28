31a40605510081aaf1fb9c013e43b819d9a8cd8b
umesh123544 <umesh.saral88@gmail.com>
2025-05-28 14:07:29 +0545

    ss

--- Backend/server.js
+++ Backend/server.js
@@ -34,9 +34,10 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-const PORT = process.env.PORT || 1000;
-// Start server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // Middleware
 app.use(cors({
