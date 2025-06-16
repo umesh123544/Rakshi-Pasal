fc9c57b8c291c39de8100413b768dd6c5843d353
umesh123544 <umesh.saral88@gmail.com>
2025-06-16 17:05:50 +0545

    Update Product.js

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+// const mongoose = require('mongoose');
+// const { Schema } = mongoose;
 
-const productSchema = new Schema({
-  name: { type: String, required: true },
-  category: { 
-    type: String, 
-    required: true,
-    enum: ['whiskey', 'wine', 'beer', 'vodka', 'cigarette']
-  },
-  description: { type: String },
-  price: { type: Number, required: true },
-  image_url: { type: String },
-  volume: { type: String },
-  is_best_seller: { type: Boolean, default: false },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
-});
+// const productSchema = new Schema({
+//   name: { type: String, required: true },
+//   category: { 
+//     type: String, 
+//     required: true,
+//     enum: ['whiskey', 'wine', 'beer', 'vodka', 'cigarette']
+//   },
+//   description: { type: String },
+//   price: { type: Number, required: true },
+//   image_url: { type: String },
+//   volume: { type: String },
+//   is_best_seller: { type: Boolean, default: false },
+//   created_at: { type: Date, default: Date.now },
+//   updated_at: { type: Date, default: Date.now }
+// });
 
-const Product = mongoose.model('Product', productSchema);
+// const Product = mongoose.model('Product', productSchema);
 
-module.exports = Product;
+// module.exports = Product;
