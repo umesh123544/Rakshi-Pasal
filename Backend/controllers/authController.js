63b4fce7382a459dedc58d1efdc37a5811baeebe
umesh123544 <umesh.saral88@gmail.com>
2025-06-16 17:09:49 +0545

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,223 +1,223 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
-const { validationResult } = require('express-validator');
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcryptjs');
+// const User = require('../models/User');
+// const { validationResult } = require('express-validator');
 
-// Register a new user
-exports.register = async (req, res) => {
-  try {
-    await User.deleteMany({ email: null });
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+// // Register a new user
+// exports.register = async (req, res) => {
+//   try {
+//     await User.deleteMany({ email: null });
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
 
-    const { username, email, first_name, last_name, password } = req.body;
-    console.log(req.body)
-    // Check if user exists
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
+//     const { username, email, first_name, last_name, password } = req.body;
+//     console.log(req.body)
+//     // Check if user exists
+//     let user = await User.findOne({ email });
+//     if (user) {
+//       return res.status(400).json({ message: 'User already exists' });
+//     }
 
-    // Create new user
-    user = new User({
-      username,
-      email,
-      first_name,
-      last_name,
-      password
-    });
+//     // Create new user
+//     user = new User({
+//       username,
+//       email,
+//       first_name,
+//       last_name,
+//       password
+//     });
 
-    // Save user (password will be hashed by pre-save hook)
-    await user.save();
+//     // Save user (password will be hashed by pre-save hook)
+//     await user.save();
 
-    // Generate JWT token
-    const payload = {
-      user: {
-        id: user.id
-      }
-    };
+//     // Generate JWT token
+//     const payload = {
+//       user: {
+//         id: user.id
+//       }
+//     };
 
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: 300 },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-};
+//     jwt.sign(
+//       payload,
+//       process.env.JWT_SECRET,
+//       { expiresIn: 300 },
+//       (err, token) => {
+//         if (err) throw err;
+//         res.json({ token });
+//       }
+//     );
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server error');
+//   }
+// };
 
-// Login user
-exports.login = async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+// // Login user
+// exports.login = async (req, res) => {
+//   try {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
 
-    const { email, password } = req.body;
+//     const { email, password } = req.body;
 
-    // Check if user exists
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: 'Invalid credentials' });
-    }
+//     // Check if user exists
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(400).json({ message: 'Invalid credentials' });
+//     }
 
-    // Check password
-    const isMatch = await user.comparePassword(password);
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid credentials' });
-    }
+//     // Check password
+//     const isMatch = await user.comparePassword(password);
+//     if (!isMatch) {
+//       return res.status(400).json({ message: 'Invalid credentials' });
+//     }
 
-    // Generate JWT token
-    const payload = {
-      user: {
-        id: user.id
-      }
-    };
+//     // Generate JWT token
+//     const payload = {
+//       user: {
+//         id: user.id
+//       }
+//     };
 
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: '7d' },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-};
+//     jwt.sign(
+//       payload,
+//       process.env.JWT_SECRET,
+//       { expiresIn: '7d' },
+//       (err, token) => {
+//         if (err) throw err;
+//         res.json({ token });
+//       }
+//     );
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server error');
+//   }
+// };
 
-// Get logged in user
-exports.getUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-};
+// // Get logged in user
+// exports.getUser = async (req, res) => {
+//   try {
+//     const user = await User.findById(req.user.id).select('-password');
+//     res.json(user);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server error');
+//   }
+// };
 
-const signToken = id => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
-  });
-};
+// const signToken = id => {
+//   return jwt.sign({ id }, process.env.JWT_SECRET, {
+//     expiresIn: process.env.JWT_EXPIRES_IN
+//   });
+// };
 
-exports.register = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
+// exports.register = async (req, res, next) => {
+//   try {
+//     const { username, password } = req.body;
     
-    const newUser = await User.create({
-      username,
-      password
-    });
+//     const newUser = await User.create({
+//       username,
+//       password
+//     });
 
-    const token = signToken(newUser._id);
+//     const token = signToken(newUser._id);
 
-    res.status(201).json({
-      status: 'success',
-      token,
-      data: {
-        user: newUser
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err.message
-    });
-  }
-};
+//     res.status(201).json({
+//       status: 'success',
+//       token,
+//       data: {
+//         user: newUser
+//       }
+//     });
+//   } catch (err) {
+//     res.status(400).json({
+//       status: 'fail',
+//       message: err.message
+//     });
+//   }
+// };
 
-exports.login = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
+// exports.login = async (req, res, next) => {
+//   try {
+//     const { username, password } = req.body;
 
-    // 1) Check if username and password exist
-    if (!username || !password) {
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Please provide username and password'
-      });
-    }
+//     // 1) Check if username and password exist
+//     if (!username || !password) {
+//       return res.status(400).json({
+//         status: 'fail',
+//         message: 'Please provide username and password'
+//       });
+//     }
 
-    // 2) Check if user exists && password is correct
-    const user = await User.findOne({ username }).select('+password');
+//     // 2) Check if user exists && password is correct
+//     const user = await User.findOne({ username }).select('+password');
 
-    if (!user || !(await user.comparePassword(password, user.password))) {
-      return res.status(401).json({
-        status: 'fail',
-        message: 'Incorrect username or password'
-      });
-    }
+//     if (!user || !(await user.comparePassword(password, user.password))) {
+//       return res.status(401).json({
+//         status: 'fail',
+//         message: 'Incorrect username or password'
+//       });
+//     }
 
-    // 3) If everything ok, send token to client
-    const token = signToken(user._id);
+//     // 3) If everything ok, send token to client
+//     const token = signToken(user._id);
 
-    res.status(200).json({
-      status: 'success',
-      token,
-      data: {
-        user
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err.message
-    });
-  }
-};
+//     res.status(200).json({
+//       status: 'success',
+//       token,
+//       data: {
+//         user
+//       }
+//     });
+//   } catch (err) {
+//     res.status(400).json({
+//       status: 'fail',
+//       message: err.message
+//     });
+//   }
+// };
 
-exports.protect = async (req, res, next) => {
-  try {
-    // 1) Getting token and check if it's there
-    let token;
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith('Bearer')
-    ) {
-      token = req.headers.authorization.split(' ')[1];
-    }
+// exports.protect = async (req, res, next) => {
+//   try {
+//     // 1) Getting token and check if it's there
+//     let token;
+//     if (
+//       req.headers.authorization &&
+//       req.headers.authorization.startsWith('Bearer')
+//     ) {
+//       token = req.headers.authorization.split(' ')[1];
+//     }
 
-    if (!token) {
-      return res.status(401).json({
-        status: 'fail',
-        message: 'You are not logged in! Please log in to get access.'
-      });
-    }
+//     if (!token) {
+//       return res.status(401).json({
+//         status: 'fail',
+//         message: 'You are not logged in! Please log in to get access.'
+//       });
+//     }
 
-    // 2) Verification token
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+//     // 2) Verification token
+//     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-    // 3) Check if user still exists
-    const currentUser = await User.findById(decoded.id);
-    if (!currentUser) {
-      return res.status(401).json({
-        status: 'fail',
-        message: 'The user belonging to this token does no longer exist.'
-      });
-    }
+//     // 3) Check if user still exists
+//     const currentUser = await User.findById(decoded.id);
+//     if (!currentUser) {
+//       return res.status(401).json({
+//         status: 'fail',
+//         message: 'The user belonging to this token does no longer exist.'
+//       });
+//     }
 
-    // GRANT ACCESS TO PROTECTED ROUTE
-    req.user = currentUser;
-    next();
-  } catch (err) {
-    res.status(401).json({
-      status: 'fail',
-      message: err.message
-    });
-  }
-};
+//     // GRANT ACCESS TO PROTECTED ROUTE
+//     req.user = currentUser;
+//     next();
+//   } catch (err) {
+//     res.status(401).json({
+//       status: 'fail',
+//       message: err.message
+//     });
+//   }
+// };
